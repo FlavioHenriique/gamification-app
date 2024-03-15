27b9aa0a04dca0fb5e3ebb49563abf3542466d0a
FlavioHenriique <flaviohenrique638@gmail.com>
2024-03-15 14:47:02 -0300

    commit

--- gamification/app/src/main/java/io/github/gamification/adapter/AdapterInsigniaItem.java
+++ gamification/app/src/main/java/io/github/gamification/adapter/AdapterInsigniaItem.java
@@ -1,12 +1,15 @@
 package io.github.gamification.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -58,17 +61,22 @@ public class AdapterInsigniaItem extends RecyclerView.Adapter<AdapterInsigniaIte
         TextView insigniaItemDescricao;
         TextView insigniaItemPercentual;
 
+        ConstraintLayout insigniaItemLayout;
+
         InsigniaItemViewHolder(@NonNull View itemView) {
             super(itemView);
             insigniaItemTitulo = itemView.findViewById(R.id.insigniaItemTitulo);
             insigniaItemDescricao = itemView.findViewById(R.id.insigniaItemDescricao);
             insigniaItemPercentual = itemView.findViewById(R.id.insigniaItemPercentual);
+            insigniaItemLayout = itemView.findViewById(R.id.insigniaItemLayout);
         }
 
         void setData(Insignia item) {
             insigniaItemTitulo.setText(item.getNome());
             insigniaItemDescricao.setText(item.getDescricao());
             insigniaItemPercentual.setText(item.getPercentualUsuarios() + "%");
+            if (item.isConquistada())
+                insigniaItemLayout.setBackgroundColor(Color.parseColor("#DCDCDC"));
         }
 
         void setListners() {
