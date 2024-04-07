d3a8bf1740e6b73898787b5af5434188ba5c1556
FlavioHenriique <flaviohenrique638@gmail.com>
2024-04-07 15:18:53 -0300

    Adiciona imagens mapa e insígnias

--- gamification/app/src/main/java/io/github/gamification/config/RetrofitInstance.java
+++ gamification/app/src/main/java/io/github/gamification/config/RetrofitInstance.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.5.9:8080/";
+    private static final String BASE_URL = "http://192.168.5.31:8080/";
 
     public static Retrofit getRetrofitInstance() {
         OkHttpClient client = new OkHttpClient.Builder().build();
